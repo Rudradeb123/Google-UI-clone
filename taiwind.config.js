9ffb55a8aa515ad5690c1f398f002d7d5288c984
RUDRADEB DAS <115949210+Rudradeb123@users.noreply.github.com>
2025-06-20 19:48:24 +0530

    Add files via upload

---
+++ taiwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+    content: ["./*.html", "./src/**/*.{html,js}"], // update based on your files
+    theme: {
+      extend: {
+        fontFamily: {
+          poppins: ['Poppins', 'sans-serif'],
+        },
+      },
+    },
+    plugins: [],
+  }
+  
